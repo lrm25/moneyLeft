e83ebcdfe578f23581a7d3783475dab7b75d8125
Luke McCrone <luke.r.mccrone@gmail.com>
2023-01-05 19:24:16 -0300

    additional system tests

--- main_system_test.go
+++ main_system_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func execProgram(t *testing.T, fileLocation string) *bytes.Buffer {
+func execProgram(t *testing.T, fileLocation string) string {
 
 	wd, err := os.Getwd()
 	require.NoError(t, err)
@@ -25,35 +25,61 @@ func execProgram(t *testing.T, fileLocation string) *bytes.Buffer {
 
 	var stdout bytes.Buffer
 	cmd.Stdout = &stdout
-
 	err = cmd.Run()
 	require.NoError(t, err)
-	return &stdout
+
+	stdoutBytes := stdout.Bytes()
+	return string(stdoutBytes)
+}
+
+func getJsonFileName(t *testing.T) string {
+	testJsonFile := t.Name() + "_" + time.Now().Format("20060102150405") + ".json"
+	return filepath.Join(os.TempDir(), testJsonFile)
 }
 
 func Test_main_fileNotFound(t *testing.T) {
 
-	stdout := execProgram(t, "nowhere")
-	stdoutBytes := stdout.Bytes()
-	stdoutString := string(stdoutBytes)
+	stdoutString := execProgram(t, "nowhere")
 	t.Log(stdoutString)
 
 	require.Contains(t, stdoutString, "The system cannot find the file specified")
 }
 
+func Test_main_moneyPerMonthJsonFormat(t *testing.T) {
+
+	testJsonFullPath := getJsonFileName(t)
+	jsonBytes := []byte("{\"totalMoney\": \"500\"}")
+	err := os.WriteFile(testJsonFullPath, jsonBytes, 0644)
+	require.NoError(t, err)
+
+	stdoutString := execProgram(t, testJsonFullPath)
+	t.Log(stdoutString)
+
+	require.Contains(t, stdoutString, "json: cannot unmarshal")
+}
+
 func Test_main_moneyPerMonthLessThanZero(t *testing.T) {
 
-	testJsonFile := "moneyLeft_test_" + time.Now().Format("20060102150405") + ".json"
-	testJsonFullPath := filepath.Join(os.TempDir(), testJsonFile)
+	testJsonFullPath := getJsonFileName(t)
 	// omit required parameters
 	jsonBytes := []byte("{\"totalMoney\": 500}")
 	err := os.WriteFile(testJsonFullPath, jsonBytes, 0644)
 	require.NoError(t, err)
 
-	stdout := execProgram(t, testJsonFullPath)
-	stdoutBytes := stdout.Bytes()
-	stdoutString := string(stdoutBytes)
+	stdoutString := execProgram(t, testJsonFullPath)
 	t.Log(stdoutString)
 
 	require.Equal(t, ERROR_MONEYPERMONTH, stdoutString, "Errors should match")
+}
+
+func Test_main_valid(t *testing.T) {
+
+	testJsonFullPath := getJsonFileName(t)
+	// omit required parameters
+	jsonBytes := []byte("{\"totalMoney\": 500, \"moneyPerMonth\": 100}")
+	err := os.WriteFile(testJsonFullPath, jsonBytes, 0644)
+	require.NoError(t, err)
+
+	stdoutString := execProgram(t, testJsonFullPath)
+	require.Contains(t, stdoutString, "5.00")
 }
