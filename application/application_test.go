2e3c5f8027323555d8257ebe9fdb3f03406d9c11
Luke McCrone <luke.r.mccrone@gmail.com>
2023-10-20 21:58:29 -0300

    linter fix

--- application/application_test.go
+++ application/application_test.go
@@ -17,5 +17,5 @@ func Test_runAgeLoop(t *testing.T) {
 	person := models.NewPerson(50, 0, 60, 1001.00, 0.00)
 	person.WithAccounts(nil, models.PositiveAccounts{models.NewAccountNoInterest("test", 1000.00)}, nil)
 	runAgeLoop(person, 2000, 1)
-	require.Contains(t, string(buffer.Bytes()), "Broke on year 2000, month 2 (age 50 years, 1 months)")
+	require.Contains(t, buffer.String(), "Broke on year 2000, month 2 (age 50 years, 1 months)")
 }
